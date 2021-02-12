735197915292929aa9e2ea9b646e2176aa93f372
Marcus Rowe <undisbeliever@gmail.com>
2021-02-12 23:01:07 +1000

    Add test that writes 0x0f to $802100 with long addressing

---
+++ src/inidisp_d7/inidisp_d7_hammer_0f_long.asm
@@ -0,0 +1,33 @@
+// Constantly writes 0x0f to address $802100
+//
+// Copyright (c) 2021, Marcus Rowe <undisbeliever@gmail.com>.
+// Distributed under The MIT License: https://opensource.org/licenses/MIT
+
+
+define ROM_NAME = "INIDISP HAMMER TEST"
+define VERSION = 1
+
+include "_inidisp_d7_common.inc"
+
+
+au()
+iu()
+code()
+function Main {
+    rep     #$30
+    sep     #$20
+a8()
+i16()
+    sei
+
+    SetupPpu()
+
+
+    lda.b   #0x0f
+
+    MainLoop:
+        sta.l   0x802100
+
+        bra     MainLoop
+}
+
