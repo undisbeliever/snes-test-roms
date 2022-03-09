6bb1f0867502a0ccd7f7ac5fa13acb854a3b4053
Marcus Rowe <undisbeliever@gmail.com>
2022-03-09 16:05:52 +1000

    Add inidisp_hammer_long_0f2100 test ROM
    This test confirms there are no corrupted tiles when using the
    `sta $0f2100` mitigation to the INIDISP open bus glitch.

---
+++ src/inidisp-glitch/inidisp_hammer_long_0f2100.asm
@@ -0,0 +1,36 @@
+// Constantly writes 0x0f to address $0f2100
+//
+// This test confirms there are no corrupted tiles when using the
+// `sta $0f2100` mitigation to the INIDISP open bus glitch.
+//
+// Copyright (c) 2021, Marcus Rowe <undisbeliever@gmail.com>.
+// Distributed under The MIT License: https://opensource.org/licenses/MIT
+
+
+define ROM_NAME = "INIDISP HAMMER TEST"
+define VERSION = 1
+
+include "_inidisp_d7_common.inc"
+
+
+au()
+iu()
+code()
+function Main {
+    rep     #$30
+    sep     #$20
+a8()
+i16()
+    sei
+
+    SetupPpu()
+
+
+    lda.b   #0x0f
+
+    MainLoop:
+        sta.l   $0f2100
+
+        bra     MainLoop
+}
+
