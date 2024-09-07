028f92ee53a03b94bd812de4abe14d6f6233b3b7
Marcus Rowe <undisbeliever@gmail.com>
2024-09-07 11:58:31 +1000

    Relicense snes-test-roms under the zlib License
    I am the sole author of this code, as confirmed by
    `git shortlog --summary --numbered --email --all`.
    
    I am also the sole author the code that was imported from the following
    projects:
     * untech-engine
     * unnamed-snes-game
    
    This commit also adds SPDX-License-Identifier tags.

--- tools/write-sfc-checksum.py
+++ tools/write-sfc-checksum.py
@@ -12,29 +12,28 @@
 # WARNING: This script will modify the input file.
 #
 #
-# SPDX-FileCopyrightText: © 2022 Marcus Rowe <undisbeliever@gmail.com
+# SPDX-FileCopyrightText: © 2022 Marcus Rowe <undisbeliever@gmail.com>
+# SPDX-License-Identifier: Zlib
 #
-# Distributed under the MIT License (MIT)
+# Copyright © 2022 Marcus Rowe <undisbeliever@gmail.com>
 #
-# Copyright (c) 2022, Marcus Rowe <undisbeliever@gmail.com>
+# This software is provided 'as-is', without any express or implied warranty.
+# In no event will the authors be held liable for any damages arising from the
+# use of this software.
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+# Permission is granted to anyone to use this software for any purpose, including
+# commercial applications, and to alter it and redistribute it freely, subject to
+# the following restrictions:
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+#    1. The origin of this software must not be misrepresented; you must not
+#       claim that you wrote the original software. If you use this software in
+#       a product, an acknowledgment in the product documentation would be
+#       appreciated but is not required.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+#    2. Altered source versions must be plainly marked as such, and must not be
+#       misrepresented as being the original software.
+#
+#    3. This notice may not be removed or altered from any source distribution.
 
 
 import os.path
@@ -188,7 +187,7 @@ def parse_arguments():
     parser = argparse.ArgumentParser(
                 allow_abbrev=False,
                 description='Calculates and writes the SNES header checksum into the header of a homebrew SNES ROM.',
-                epilog='Distributed under the MIT License,  see the script source code for more details.')
+                epilog='Distributed under the zlib License,  see the script source code for more details.')
 
     mgroup = parser.add_mutually_exclusive_group(required=True)
     mgroup.add_argument('--lorom', action="store_true",
